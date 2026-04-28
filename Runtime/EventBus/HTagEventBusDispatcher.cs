006cb1c6733f7dfda7cd3cb88845997be111ec3c
Edward Brave <edward.brave45@gmail.com>
2026-04-28 22:22:49 +0300

    refactor event handling in HTagEventBusDispatcher

--- Runtime/EventBus/HTagEventBusDispatcher.cs
+++ Runtime/EventBus/HTagEventBusDispatcher.cs
@@ -9,20 +9,25 @@ namespace HTags.EventBus
         private BaseHTagField hTagField;
         
         public UnityEvent onTagEvent = new ();
+
+        public void Raise()
+        {
+            HTagEventBus.Raise(hTagField.BaseTag);
+        }
         
-        private void Raise()
+        private void OnRaised()
         {
             onTagEvent.Invoke();
         }
         
         private void OnEnable()
         {
-            HTagEventBus.AddListener(hTagField.BaseTag, Raise);
+            HTagEventBus.AddListener(hTagField.BaseTag, OnRaised);
         }
         
         private void OnDisable()
         {
-            HTagEventBus.RemoveListener(hTagField.BaseTag, Raise);
+            HTagEventBus.RemoveListener(hTagField.BaseTag, OnRaised);
         }
     }
 }
